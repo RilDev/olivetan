8ab1af76e3f2cf8505d67b8ba566d081fcdbe2d2
RilDev <c.public@tutanota.com>
2021-01-28 10:57:02 +0100

    start modernizer script

---
+++ scripts/modernize-transcription.nim
@@ -0,0 +1,22 @@
+# install nim: sudo apt install nim
+# This script has for objective to automatically modernize the transcript of the Olivetan Bible.
+
+# Replace all old letters
+## Replace long s
+## Replace scribal abreviations
+## Correct "nm" combianation to "mm" for words such as "homme" or "comme"
+## Replace "n" to "m" for words such as "hone" -> "home" & "cone" -> "come"
+## Translate "i" to "j" for the following list of words. ex: "iamais" -> "jamais"
+
+# Modernize punctuation
+## ".": no space before, add space after
+## ",": no space before, add space after
+## "/" -> ";": no space before, add space after
+## ":": add space before, add space after
+## "(": add space before, no space after
+## ")": no space before, add space after
+## "&": add space before, add space after
+
+# Remove all double spaces
+
+# Log out all words and their frequency to ease the search of typos
