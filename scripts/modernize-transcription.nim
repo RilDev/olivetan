eb805fdb0fec64dd69083c2fec496bb263eb5747
RilDev <c.public@tutanota.com>
2021-01-29 11:46:50 +0100

    add superscript and asterix to rules

--- scripts/modernize-transcription.nim
+++ scripts/modernize-transcription.nim
@@ -6,7 +6,9 @@
 ## Replace scribal abreviations
 ## Correct "nm" combianation to "mm" for words such as "homme" or "comme"
 ## Replace "n" to "m" for words such as "hone" -> "home" & "cone" -> "come"
-## Translate "i" to "j" for the following list of words. ex: "iamais" -> "jamais"
+## Replace "i" to "j" for the following list of words. ex: "iamais" -> "jamais"
+## Replace "u" to "v" for the following list of words. ex: "suyuant" -> "suyvant"
+## Replace "v" to "u" for the following list of words. ex: "suyuant" -> "vni"
 
 # Modernize punctuation
 ## ".": no space before, add space after
@@ -17,6 +19,11 @@
 ## ")": no space before, add space after
 ## "&": add space before, add space after
 
+# Remove superscript letters
+
+# Remove asterixes
+
 # Remove all double spaces
 
 # Log out all words and their frequency to ease the search of typos
+# The words with the smallest frequecy come on top of the list
