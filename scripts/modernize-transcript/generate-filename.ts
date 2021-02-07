0df6e02987593f9adc94ad11e07711fe6870b62c
RilDev <c.public@tutanota.com>
2021-02-07 15:59:02 +0100

    formated

--- scripts/modernize-transcript/generate-filename.ts
+++ scripts/modernize-transcript/generate-filename.ts
@@ -13,38 +13,50 @@ Todo:
 - [x] Add assertEquals in the Error tests to make sure the error message displays correctly
 */
 
-export const generateFilename = (filename: string, stringToAppend: string): string => {
-    /** Errors **/
-    /* filename or stringToAppend are empty strings */
-    if (filename === '' || stringToAppend === '') {
-        throw Error('Parameters must not be empty strings!');
-    }
-    /* filename only contains one period "." */
-    if (filename === '.') {
-        throw Error('filename must be of the format: filename.ext');
-    }
-    /* filename has more than one period */
-    if (filename.split('.').length !== 2) {
-        throw Error('filename, the first parameter, must contain exactly 1 period "."!');
-    }
-    /* stringToAppend contains a period */
-    if (stringToAppend.split('.').length !== 1) {
-        throw Error('stringToAppend, the second parameter, must not contain any period "."!')
-    }
-    /* filename or stringToAppend contains illigal characters (not [^A-Za-z0-9\-_\.]) */
-    if (filename.match(/[^A-Za-z0-9\-_\.]/) || stringToAppend.match(/[^A-Za-z0-9\-_\.]/)) {
-        throw Error('Spaces and special characters not allowed! Only characters in [A-Za-z0-9\-_\.] are allowed.')
-    }
+export const generateFilename = (
+  filename: string,
+  stringToAppend: string,
+): string => {
+  /** Errors **/
+  /* filename or stringToAppend are empty strings */
+  if (filename === "" || stringToAppend === "") {
+    throw Error("Parameters must not be empty strings!");
+  }
+  /* filename only contains one period "." */
+  if (filename === ".") {
+    throw Error("filename must be of the format: filename.ext");
+  }
+  /* filename has more than one period */
+  if (filename.split(".").length !== 2) {
+    throw Error(
+      'filename, the first parameter, must contain exactly 1 period "."!',
+    );
+  }
+  /* stringToAppend contains a period */
+  if (stringToAppend.split(".").length !== 1) {
+    throw Error(
+      'stringToAppend, the second parameter, must not contain any period "."!',
+    );
+  }
+  /* filename or stringToAppend contains illigal characters (not [^A-Za-z0-9\-_\.]) */
+  if (
+    filename.match(/[^A-Za-z0-9\-_\.]/) ||
+    stringToAppend.match(/[^A-Za-z0-9\-_\.]/)
+  ) {
+    throw Error(
+      "Spaces and special characters not allowed! Only characters in [A-Za-z0-9\-_\.] are allowed.",
+    );
+  }
 
-    /** Generate New Filename **/
-    let generatedFilename: string = '';
-    /* Get filename and extension */
-    const filenameList: string[] = filename.split('.');
-    const filenameName: string = filenameList[0];
-    const filenameExtension: string = filenameList[1];
-    /* Append `-${appendToFilename}` */
-    generatedFilename = `${filenameName}-${stringToAppend}.${filenameExtension}`;
+  /** Generate New Filename **/
+  let generatedFilename: string = "";
+  /* Get filename and extension */
+  const filenameList: string[] = filename.split(".");
+  const filenameName: string = filenameList[0];
+  const filenameExtension: string = filenameList[1];
+  /* Append `-${appendToFilename}` */
+  generatedFilename = `${filenameName}-${stringToAppend}.${filenameExtension}`;
 
-    /** Return newly generated filename **/
-    return generatedFilename;
-}
+  /** Return newly generated filename **/
+  return generatedFilename;
+};
--- scripts/modernize-transcript/tests/generate-filename.test.ts
+++ scripts/modernize-transcript/tests/generate-filename.test.ts
@@ -1,65 +1,129 @@
-import { assertEquals, assertThrows } from "https://deno.land/std/testing/asserts.ts";
+import {
+  assertEquals,
+  assertThrows,
+} from "https://deno.land/std/testing/asserts.ts";
 import { generateFilename } from "../generate-filename.ts";
 
 /** Errors **/
-Deno.test('filename or stringToAppend are empty strings', () => {
-    const error1 = assertThrows(() => generateFilename('',''));
-    const error2 = assertThrows(() => generateFilename('test.txt',''));
-    const error3 = assertThrows(() => generateFilename('','modernized'));
+Deno.test("filename or stringToAppend are empty strings", () => {
+  const error1 = assertThrows(() => generateFilename("", ""));
+  const error2 = assertThrows(() => generateFilename("test.txt", ""));
+  const error3 = assertThrows(() => generateFilename("", "modernized"));
 
-    assertEquals(error1.message, 'Parameters must not be empty strings!');
-    assertEquals(error2.message, 'Parameters must not be empty strings!');
-    assertEquals(error3.message, 'Parameters must not be empty strings!');
-})
+  assertEquals(error1.message, "Parameters must not be empty strings!");
+  assertEquals(error2.message, "Parameters must not be empty strings!");
+  assertEquals(error3.message, "Parameters must not be empty strings!");
+});
 
 Deno.test('filename only contains one period "."', () => {
-    const error1 = assertThrows(() => generateFilename('.','modernized'));
+  const error1 = assertThrows(() => generateFilename(".", "modernized"));
 
-    assertEquals(error1.message, 'filename must be of the format: filename.ext');
+  assertEquals(error1.message, "filename must be of the format: filename.ext");
 });
 
-Deno.test('filename has more than one period', () => {
-    const error1 = assertThrows(() => generateFilename('test.test.txt','modernized'));
-    const error2 = assertThrows(() => generateFilename('t.est.test.txt','modernized'));
+Deno.test("filename has more than one period", () => {
+  const error1 = assertThrows(() =>
+    generateFilename("test.test.txt", "modernized")
+  );
+  const error2 = assertThrows(() =>
+    generateFilename("t.est.test.txt", "modernized")
+  );
 
-    assertEquals(error1.message, 'filename, the first parameter, must contain exactly 1 period "."!');
-    assertEquals(error2.message, 'filename, the first parameter, must contain exactly 1 period "."!');
+  assertEquals(
+    error1.message,
+    'filename, the first parameter, must contain exactly 1 period "."!',
+  );
+  assertEquals(
+    error2.message,
+    'filename, the first parameter, must contain exactly 1 period "."!',
+  );
 });
 
-Deno.test('stringToAppend contains a period', () => {
-    const error1 = assertThrows(() => generateFilename('test.txt','modernized.md'));
-    const error2 = assertThrows(() => generateFilename('test.txt','modernized.txt'));
-    const error3 = assertThrows(() => generateFilename('test.txt','modernized.test.txt'));
+Deno.test("stringToAppend contains a period", () => {
+  const error1 = assertThrows(() =>
+    generateFilename("test.txt", "modernized.md")
+  );
+  const error2 = assertThrows(() =>
+    generateFilename("test.txt", "modernized.txt")
+  );
+  const error3 = assertThrows(() =>
+    generateFilename("test.txt", "modernized.test.txt")
+  );
 
-    assertEquals(error1.message, 'stringToAppend, the second parameter, must not contain any period "."!');
-    assertEquals(error2.message, 'stringToAppend, the second parameter, must not contain any period "."!');
-    assertEquals(error3.message, 'stringToAppend, the second parameter, must not contain any period "."!');
+  assertEquals(
+    error1.message,
+    'stringToAppend, the second parameter, must not contain any period "."!',
+  );
+  assertEquals(
+    error2.message,
+    'stringToAppend, the second parameter, must not contain any period "."!',
+  );
+  assertEquals(
+    error3.message,
+    'stringToAppend, the second parameter, must not contain any period "."!',
+  );
 });
 
-Deno.test('filename or stringToAppend contains illigal characters (not [^A-Za-z0-9\-_\.])', () => {
-    const error1 = assertThrows(() => generateFilename('test 2.txt','modernizer'));
-    const error2 = assertThrows(() => generateFilename('!test.txt','modernizer'));
-    const error3 = assertThrows(() => generateFilename('test.txt','modernizer 2'));
-    const error4 = assertThrows(() => generateFilename('test.txt','modernizer!'));
-    const error5 = assertThrows(() => generateFilename('test | Official.txt','modernizer'));
-    const error6 = assertThrows(() => generateFilename('tes?t.txt','moder/nizer'));
+Deno.test("filename or stringToAppend contains illigal characters (not [^A-Za-z0-9\-_\.])", () => {
+  const error1 = assertThrows(() =>
+    generateFilename("test 2.txt", "modernizer")
+  );
+  const error2 = assertThrows(() =>
+    generateFilename("!test.txt", "modernizer")
+  );
+  const error3 = assertThrows(() =>
+    generateFilename("test.txt", "modernizer 2")
+  );
+  const error4 = assertThrows(() =>
+    generateFilename("test.txt", "modernizer!")
+  );
+  const error5 = assertThrows(() =>
+    generateFilename("test | Official.txt", "modernizer")
+  );
+  const error6 = assertThrows(() =>
+    generateFilename("tes?t.txt", "moder/nizer")
+  );
 
-    assertEquals(error1.message, 'Spaces and special characters not allowed! Only characters in [A-Za-z0-9\-_\.] are allowed.');
-    assertEquals(error2.message, 'Spaces and special characters not allowed! Only characters in [A-Za-z0-9\-_\.] are allowed.');
-    assertEquals(error3.message, 'Spaces and special characters not allowed! Only characters in [A-Za-z0-9\-_\.] are allowed.');
-    assertEquals(error4.message, 'Spaces and special characters not allowed! Only characters in [A-Za-z0-9\-_\.] are allowed.');
-    assertEquals(error5.message, 'Spaces and special characters not allowed! Only characters in [A-Za-z0-9\-_\.] are allowed.');
-    assertEquals(error6.message, 'Spaces and special characters not allowed! Only characters in [A-Za-z0-9\-_\.] are allowed.');
+  assertEquals(
+    error1.message,
+    "Spaces and special characters not allowed! Only characters in [A-Za-z0-9\-_\.] are allowed.",
+  );
+  assertEquals(
+    error2.message,
+    "Spaces and special characters not allowed! Only characters in [A-Za-z0-9\-_\.] are allowed.",
+  );
+  assertEquals(
+    error3.message,
+    "Spaces and special characters not allowed! Only characters in [A-Za-z0-9\-_\.] are allowed.",
+  );
+  assertEquals(
+    error4.message,
+    "Spaces and special characters not allowed! Only characters in [A-Za-z0-9\-_\.] are allowed.",
+  );
+  assertEquals(
+    error5.message,
+    "Spaces and special characters not allowed! Only characters in [A-Za-z0-9\-_\.] are allowed.",
+  );
+  assertEquals(
+    error6.message,
+    "Spaces and special characters not allowed! Only characters in [A-Za-z0-9\-_\.] are allowed.",
+  );
 });
 
 /** Checks **/
 
 /** General Tests **/
 Deno.test("Generate Filename from well formatted strings", () => {
-    assertEquals(generateFilename('test.txt','modernized'), 'test-modernized.txt');
-    assertEquals(generateFilename('test.txt','wfl'), 'test-wfl.txt');
-    assertEquals(generateFilename('test.md','modernized'), 'test-modernized.md');
-    assertEquals(generateFilename('test.md','wfl'), 'test-wfl.md');
-    assertEquals(generateFilename('Test.txt','modernized'), 'Test-modernized.txt');
-    assertEquals(generateFilename('a.b','c'), 'a-c.b');
+  assertEquals(
+    generateFilename("test.txt", "modernized"),
+    "test-modernized.txt",
+  );
+  assertEquals(generateFilename("test.txt", "wfl"), "test-wfl.txt");
+  assertEquals(generateFilename("test.md", "modernized"), "test-modernized.md");
+  assertEquals(generateFilename("test.md", "wfl"), "test-wfl.md");
+  assertEquals(
+    generateFilename("Test.txt", "modernized"),
+    "Test-modernized.txt",
+  );
+  assertEquals(generateFilename("a.b", "c"), "a-c.b");
 });
